b8b597e7e6813677cbe2b7848015911480b5f203
Prashant Sharma <116828172+Prashantsharma3@users.noreply.github.com>
2023-05-03 12:49:00 +0530

    hi

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-function getWeather() {
-  const city = document.getElementById("city").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const apiKey = 'b81ffbf5d8010c43f2ec3c28def9e268';
+const searchButton = document.querySelector('button');
+const cityName = document.querySelector('#city-name');
+const temperature = document.querySelector('#temperature');
+const humidity = document.querySelector('#humidity');
+const description = document.querySelector('#description');
+const forecast = document.querySelector('#forecast');
+const cityImage = document.querySelector('#city-image');
+const weatherImage = document.querySelector('#weather-image');
 
+function getWeather() {
+  const cityInput = document.querySelector('#city').value;
+  const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${cityInput}`;
+  
   fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const weather = `
-        <h2>${data.name}</h2>
-        <p>Temperature: ${data.main.temp}°C</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        <p>Description: ${data.weather[0].description}</p>
-      `;
-      document.getElementById("popup-content").innerHTML = weather;
-      document.getElementById("popup").style.display = "block";
+    .then(response => response.json())
+    .then(data => {
+      // Update the weather information
+      cityName.textContent = data.location.name;
+      temperature.textContent = `Temperature: ${data.current.temperature}°C`;
+      humidity.textContent = `Humidity: ${data.current.humidity}%`;
+      description.textContent = `Description: ${data.current.weather_descriptions[0]}`;
+      forecast.textContent = `Feels like: ${data.current.feelslike}°C`;
+      cityImage.src = data.current.weather_icons[0];
+      weatherImage.src = data.current.weather_icons[0];
+
+      // Check the weather condition and update the UI accordingly
+      const isSunny = data.current.weather_descriptions[0].toLowerCase().includes('sunny');
+      const isCloudy = data.current.weather_descriptions[0].toLowerCase().includes('cloudy');
+
+      // Update the #weather-front background color
+      if (isSunny) {
+        document.querySelector('#weather-front').classList.add('sunny');
+        document.querySelector('#weather-front').classList.remove('cloudy');
+      } else if (isCloudy) {
+        document.querySelector('#weather-front').classList.add('cloudy');
+        document.querySelector('#weather-front').classList.remove('sunny');
+      }
     })
-    .catch((error) => console.error(error));
+    .catch(error => console.log(error));
 }
+
+searchButton.addEventListener('click', getWeather);
